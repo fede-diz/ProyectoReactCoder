1e3fc24c83a146b9f479a6e4d59afc18c83c9371
Nicolas Carinelli <nicolascarinelli@gmail.com>
2022-09-22 13:24:09 -0300

    Ajuste clase 3

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Ecommerce</h1>
+            <div>
+                <button>Abrigos</button>
+                <button>Accesorios</button>
+                <button>Calzados</button>
+                <button>Camisas</button>
+                <button>Pantalones</button>
+                <button>Remeras</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
