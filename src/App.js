be5191ded0118bb00eb00ac1c3df9298470ede1b
Nicolas Carinelli <nicolascarinelli@gmail.com>
2022-10-06 16:53:15 -0300

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar.js';
 import ItemCount from './components/ItemCount/ItemCount';
-import MercadoLibre from './components/MercadoLibre/MercadoLibre';
 
 function App() {               // Dentro de las llaves {} de cada componente le paso propiedades con formato JavaScript: número, variable, 'un string', unaFuncion()
 
@@ -13,7 +12,6 @@ function App() {               // Dentro de las llaves {} de cada componente le 
   return (                     // [un, array], {un:x, objeto:y, literal:z}, etc
     <div className="App">
       <Navbar />
-      <MercadoLibre />
       <ItemListContainer greeting={'BIENVENIDOS AL HIMALAYA'}/>
       <ItemCount stock={5} onAdd={handleOnAdd} />
     </div>
